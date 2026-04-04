0c6ea0111a0f532cc82f587cbfc87f707415a55a
Edu1Sinico <EduardoSenai888@gmail.com>
2026-04-03 21:45:28 -0300

    Encerrando o conteúdo dos fundamentos de C# e enviando para o Github - 03/04/2026

---
+++ 2_fundamentos_dotnet/fundamentos_dotnet_exercicio/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.ComponentModel;
+using fundamentos_dotnet_exercicio;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Contato> contatos = new List<Contato>();
+        int opcao;
+
+        do
+        {
+            Console.WriteLine("\n===== CONTATOS =====\n");
+            Console.WriteLine("1 - Adicionar contato");
+            Console.WriteLine("2 - Listar contatos");
+            Console.WriteLine("3 - Buscar por nome");
+            Console.WriteLine("4 - Excluir contato");
+            Console.WriteLine("0 - Sair");
+            Console.Write("\nEscolha: ");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0:
+                    Console.WriteLine("\nEncerrando...\n");
+                    break;
+
+                case 1:
+                    AdicionarContato(contatos);
+                    break;
+
+                case 2:
+                    ListarContatos(contatos);
+                    break;
+
+                case 3:
+                    BuscarContato(contatos);
+                    break;
+
+                case 4:
+                    ApagarContato(contatos);
+                    break;
+
+                default:
+                    Console.WriteLine("\nEscolhar uma opção válida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+    }
+
+    static void AdicionarContato(List<Contato> contatos)
+    {
+        Console.WriteLine("\n --- Cadastro de Contato ---");
+
+        Console.Write("\nNome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Telefone: ");
+        string telefone = Console.ReadLine();
+
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+
+        contatos.Add(new Contato(nome, telefone, email));
+
+        Console.WriteLine("\nContato adicionado com sucesso!");
+    }
+
+    static void ListarContatos(List<Contato> contatos)
+    {
+        Console.WriteLine("\n--- Lista de Contatos ---\n");
+
+        foreach (var c in contatos)
+        {
+            Console.WriteLine($"Nome: {c.Nome} | Tel: {c.Telefone} | Email: {c.Email}");
+        }
+    }
+
+    static void BuscarContato(List<Contato> contatos)
+    {
+        Console.WriteLine("\n --- Busca de Contato ---");
+
+        Console.Write("\nDigite o nome: ");
+        string nomeBusca = Console.ReadLine();
+
+        var encontrado = contatos
+            .FirstOrDefault(c => c.Nome.ToLower() == nomeBusca.ToLower());
+
+        if (encontrado != null)
+        {
+            Console.WriteLine("\nContato encontrado: \n");
+            Console.WriteLine($"Nome: {encontrado.Nome}");
+            Console.WriteLine($"Telefone: {encontrado.Telefone}");
+            Console.WriteLine($"Email: {encontrado.Email}");
+        }
+        else
+        {
+            Console.WriteLine("Contato não encontrado.");
+        }
+    }
+
+    static void ApagarContato(List<Contato> contatos)
+    {
+        Console.WriteLine("\n --- Apagar um Contato ---");
+
+        Console.Write("\nDigite o nome: ");
+        string nomeBusca = Console.ReadLine();
+
+        var encontrado = contatos
+            .FirstOrDefault(c => c.Nome.ToLower() == nomeBusca.ToLower());
+
+        if (encontrado != null)
+        {
+            contatos.Remove(encontrado);
+            Console.WriteLine("\nContato removido com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("\nContato não encontrado.");
+        }
+    }
+
+
+}
---
+++ 2_fundamentos_dotnet/fundamentos_dotnet_introducao/Program.cs
@@ -0,0 +1,263 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        /* 
+        • Trabalhar com Arrays e Arrays Multidimensionais
+        • Dominar Coleções Genéricas(List, Dictionary, HashSet)
+        • Manipular Strings eficientemente
+        • Criar e usar Métodos e Funções
+        • Entender Tratamento de Exceções
+        • Introdução ao LINQ(consultas em coleções) */
+
+        // ====== ARRAYS ======
+
+        // Declaração Inicial
+        int[] numeros = new int[5];
+
+        // Com valores iniciais
+        int[] nums = { 1, 2, 3, 4, 5 };
+
+        // Acessando elementos
+        int primeiro = nums[0]; // 1
+        int ultimo = nums[4]; // 5
+
+        // Tamanho do array
+        int tamanho = nums.Length; // 5
+
+        // ====== ARRAYS MULTIDIMENSIONAIS ======
+
+        // Array 2D (Matriz)
+        int[,] matriz = new int[3, 3];
+
+        // Com valores
+        int[,] mat =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        // Acessar elementos
+        int valor = mat[1, 2]; // 6
+
+        // ====== LISTAS, DICIONÁRIO & HASH SET ======
+
+        // Criar uma lista
+        List<string> nomes = new List<string>();
+
+        // Adicionar elementos
+        nomes.Add("João");
+        nomes.Add("Maria");
+
+        // Acessar
+        string primeiroNome = nomes[0]; // João
+
+        // Remover
+        nomes.Remove("João");
+        nomes.RemoveAt(0); // João
+
+        // ----------------------------------
+
+        // Criar um dicionário
+        Dictionary<string, int> idades = new Dictionary<string, int>();
+
+        // Adicionar
+        idades["João"] = 25;
+        idades.Add("Maria", 30);
+
+        // Acessar
+        int idadeDic = idades["João"];
+
+        // Verificar se existe 
+        if (idades.ContainsKey("Ana")) { }
+
+        // ----------------------------------
+
+        // HashSet, Queue e Stack
+
+        // HashSet<T> - Sem Duplicatas
+        HashSet<int> numsHash = new HashSet<int>();
+        numsHash.Add(1); numsHash.Add(1); // Só uma Vez
+
+
+        // ----------------------------------
+
+        // Queue<t> - Fila (FIFO)
+        Queue<string> fila = new Queue<string>();
+
+        fila.Enqueue("A"); // Adiciona
+        var filaPrimeiro = fila.Dequeue(); // Remove
+
+
+        // ----------------------------------
+
+        // Stack<T> - Pilha (LIFO)
+        Stack<int> pilha = new Stack<int>();
+
+        pilha.Push(1); // Empilha
+        var topo = pilha.Pop(); // Desempilha
+
+        // ====== MANIPULAÇÃO DE STRING ======
+
+        // String Interpolation
+
+        string nome = "João";
+        int idade = 25;
+
+        // Forma antiga (concatenação)
+        string msg1 = "Olá " + nome + "!";
+
+        // Forma moderna (interpolacação)
+        string msg2 = $"Olá {nome}";
+        string msg3 = $"{nome} tem {idade} anos";
+
+        // StringBuilder - Para concatenação múltiplas
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Olá");
+        sb.Append(" ");
+        sb.Append("Mundo");
+        string resultado = sb.ToString();
+
+        // ====== MÉTODOS E FUNÇÕES ======
+
+        // Métodos sem retorno (void)
+        void ImprimirMensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        // Método com retorno
+        int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Chamado métodos
+        ImprimirMensagem("Olá!");
+        int resultadoSoma = Somar(5, 3); // 8
+
+        // •-< Parâmetros Especiais >-•
+
+        // Parâmetros Opcionais
+        void Saudar(string nome, string saudacao = "Olá")
+        {
+            Console.WriteLine($"{saudacao}, {nome}!");
+        }
+
+        Saudar("João");         // Olá, João!
+        Saudar("Maria", "Oi");   // Oi, Maria!
+
+        // Parâmetros Nomeados
+        Saudar(nome: "Pedro", saudacao: "E ai");
+
+        // ref - Passagem de referência
+        void Dobrar(ref int numero)
+        {
+            numero *= 2;
+        }
+
+        int x = 5;
+        Dobrar(ref x);
+        Console.WriteLine(x);
+
+        // out - Saída obrigatória
+        // bool TryParse(string s, out int result){}
+
+        // ---------------------------------
+
+        // Sobrecarga de Métodos (Overload)
+
+        // Mesmo nome, assinaturas diferentes
+        int SomarAB(int a, int b)
+        {
+            return a + b;
+        }
+
+        /* double SomarAB(double a, double b)
+        {
+            return a + b;
+        }
+
+        int SomarAB(int a, int b, int c)
+        {
+            return a+b+c;
+        } */
+
+        // C# escolhe o método correto automaticamente!
+
+        // ====== TRATAMENTOS DE EXCEÇÃO ======
+
+        try
+        {
+            // Código que pode gerar erro
+            // int resultadoDiv = 10/0;
+        }
+        catch (DivideByZeroException ex)
+        {
+            // Trata erro específico 
+            Console.WriteLine("Não pode dividir por zero!");
+        }
+        finally
+        {
+            // SEMPRE executa (com ou sem erro)
+            Console.WriteLine("Finalizando...");
+        }
+
+
+        // Usar exceções customizadas
+        void ValidarIdade(int idade)
+        {
+            if (idade < 0 || idade > 120)
+            {
+                throw new IdadeInvalidaException("Idade deve estar entre 0 e 120.");
+            }
+        }
+
+        // ====== LINQS ======
+
+        // Sem LINQ (Forma tradicional)
+        List<int> numerosLIQN = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        List<int> paresSemLINQ = new List<int>();
+        foreach (int n in numerosLIQN)
+        {
+            if (n % 2 == 0) paresSemLINQ.Add(n);
+        }
+
+        // Com LINQ (moderno)
+
+        var paresComLINQ = numerosLIQN.Where(n => n % 2 == 0).ToList();
+        // Resultado: {2,4,6,8,10}
+
+        // --- LiNQ: Select (Transformar) ---
+
+        List<string> nomesComLINQ = new List<string> { "João", "Maria", "Pedro" };
+
+        // Transformar em maiúsculas
+        var nomesUpper = nomesComLINQ.Select(n => n.ToUpper()).ToList();
+        // Resultado: {"JOÃO","MARIA","PEDRO"}
+
+        // Transformar em tamanhos
+        var tamanhos = nomesComLINQ.Select(n => n.Length).ToList();
+        // Resultado: {4,5,5}
+
+        // Combinar Where + Select
+        var resultadoWhereSelect = numerosLIQN.Where(n => n > 5).Select(n => n * 2).ToList();
+
+    }
+
+    // Criando Exceções Customizadas
+
+    // Criar exceção customizada
+    public class IdadeInvalidaException : Exception
+    {
+        public IdadeInvalidaException(string mensagem) : base(mensagem) { }
+    }
+}
